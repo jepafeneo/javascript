dbd76f1fe3a8a95714a10f3b2e5999fe4c64dad7
Jean Paul Ferreira <jp.ferreira@neoland.es>
2026-02-17 15:19:56 -0300

    Fetch - JavaScript - Clase 13

---
+++ clase-13-fetch/js/scripts.js
@@ -0,0 +1,28 @@
+// const promise = fetch("https://fakestoreapi.com/products/1");
+
+// promise
+//   .then((response) => {
+//     console.log(response);
+
+//     return response.json();
+//   })
+//   .then((product) => console.log(product));
+
+// fetch
+
+// fetch("https://fakestoreapi.com/products/1")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+// async / await
+
+async function mostrarProducto() {
+  const response = await fetch("https://fakestoreapi.com/products/1");
+  const product = await response.json();
+  console.log(product);
+
+  document.querySelector("#titulo").textContent = product.title;
+  document.querySelector("#precio").textContent = `$ ${product.price}`;
+}
+
+mostrarProducto();
