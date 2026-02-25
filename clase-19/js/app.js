7305945cfef8e0a2e9a664e5d6a0571c0f515098
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-02-25 16:36:31 -0300

    Biblioteca - JavaScript - Clase 19

---
+++ clase-19/js/app.js
@@ -0,0 +1,484 @@
+// function capitalize(texto) {
+//   if (!texto) return "";
+//   return texto.charAt(0).toUpperCase() + texto.slice(1).toLowerCase();
+// }
+
+// class Persona {
+//   constructor(nombre) {
+//     this.nombre = nombre;
+//     this.email = null;
+//     this.admin = false;
+//   }
+
+//   saludar() {
+//     return `Hola, me llamo ${this.nombre}`;
+//   }
+
+//   setNombre(nombre) {
+//     this.nombre = nombre;
+//   }
+
+//   getNombre() {
+//     return capitalize(String(this.nombre).toLowerCase());
+//   }
+// }
+
+// const persona1 = new Persona("jUAN pAbLo");
+
+// // persona1.nombre = "Juan Pablo";
+// // persona1.setNombre("Juan Pablo");
+
+// // console.log(persona1.nombre);
+// console.log(persona1.getNombre());
+
+// // console.log(persona1);
+// // console.log(persona1.saludar());
+
+// // const persona2 = new Persona("María");
+
+// // console.log(persona2);
+// // console.log(persona2.saludar());
+
+// ---
+
+// class Producto {
+//   constructor(nombre, precio) {
+//     this.nombre = nombre;
+//     this.precio = precio;
+//     this.stock = 0;
+//   }
+
+//   addStock(cantidad) {
+//     if (cantidad < 0) return;
+//     this.stock += cantidad;
+//   }
+// }
+
+// const celular = new Producto("Motorola X", 1000);
+
+// ---
+
+// Crear clase `Libro` con:
+
+// - id
+// - titulo
+// - autor
+// - disponible (true por defecto)
+
+// class Product {
+//   constructor(id, titulo, autor, disponible = true) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = disponible;
+//   }
+// }
+
+// const Libro = new Product(1, "El Quijote", "Miguel de Cervantes");
+
+// ---
+
+// class Libro {
+//   constructor(id, titulo, autor) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = true;
+//   }
+// }
+// const libro1 = new Libro(1, "El profesor", "John Katzenbach");
+
+// console.log(libro1);
+
+// ---
+
+// class Libro {
+//   constructor(id, titulo, autor) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = true;
+//   }
+// }
+
+// const libro1 = new Libro(1, "El señor de los anillos", "J.R.R. Tolkien");
+
+// console.log(libro1);
+
+// ---
+
+// class Libro {
+//   constructor(id, titulo, autor) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = true;
+//   }
+// }
+
+// const libro1 = new Libro(22, "quijote", "cervantes");
+
+// console.log(libro1);
+
+// ---
+
+// class Libro {
+//   constructor(id, titulo, autor, disponible) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = disponible;
+//   }
+// }
+
+// const libro1 = new Libro(1, "Juventud", "anonimo", true);
+
+// console.log(libro1);
+
+// ---
+
+// class Libro {
+//   constructor(id, titulo, autor) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = true;
+//   }
+// }
+
+// const libro1 = new Libro(3, "Jean y sus secuaces", "JS");
+
+// console.log(libro1);
+
+// ---
+
+// Métodos:
+
+// - getTitulo()
+// - getAutor()
+// - isDisponible()
+// - prestar() → cambia disponible a false
+// - devolver() → cambia disponible a true
+
+// ---
+
+// class Product {
+//   constructor(id, titulo, autor, disponible = true) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = disponible;
+//   }
+
+//   getTitulo() {
+//     return this.titulo;
+//   }
+
+//   getAutor() {
+//     return this.autor;
+//   }
+
+//   isDisponible() {
+//     return this.disponible;
+//   }
+
+//   prestar() {
+//     if (this.disponible) {
+//       this.disponible = false;
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   devolver() {
+//     if (!this.disponible) {
+//       this.disponible = true;
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+
+// const Libro = new Product(1, "El Quijote", "Miguel de Cervantes");
+
+// ---
+
+// class Libro {
+//   constructor(id, titulo, autor) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = true;
+//   }
+
+//   getTitulo() {
+//     return this.titulo;
+//   }
+
+//   getAutor() {
+//     return this.autor;
+//   }
+
+//   isDisponible() {
+//     return this.disponible;
+//   }
+
+//   prestar() {
+//     this.disponible = false;
+//   }
+
+//   devolver() {
+//     if (!this.disponible) {
+//       this.disponible = true;
+//       return true;
+//     }
+//     return false;
+//   }
+// }
+
+// const libro1 = new Libro(1, "El profesor", "John Katzenbach");
+
+// libro1.prestar();
+
+// console.log(libro1.isDisponible());
+
+// ---
+
+// class Libro {
+//   constructor(id, titulo, autor) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = true;
+//   }
+//   getTitulo() {
+//     return this.titulo;
+//   }
+//   getAutor() {
+//     return this.autor;
+//   }
+//   isDisponible() {
+//     return this.disponible;
+//   }
+//   prestar() {
+//     if (this.disponible) {
+//       this.disponible = false;
+//       return "Libro prestado correctamente";
+//     } else {
+//       return "No disponible";
+//     }
+//   }
+//   devolver() {
+//     this.disponible = true;
+//     return "Libro devuelto";
+//   }
+// }
+
+// const libro1 = new Libro(22, "Quijote", "Cervantes");
+
+// ---
+
+// class Libro {
+//   getTitulo() {
+//     return String(this.titulo);
+//   }
+
+//   getAutor() {
+//     return String(this.autor);
+//   }
+
+//   isDisponible() {
+//     return Boolean(this.disponible);
+//   }
+
+//   prestar() {
+//     // if (this.disponible = false) {
+//     //     return this.cantidad = true;
+//     //     }
+//     //     else{
+//     //         return false;
+
+//     // }
+
+//     if (this.disponible == true) {
+//       this.disponible = false;
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   devolver() {
+//     // this.disponible = false
+//     // !this.disponible es true
+//     if (!this.disponible) {
+//       this.disponible = true;
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+
+// ---
+
+// class Libro {
+//   constructor(id, titulo, autor) {
+//     // estos son todos lo que necesitemos, si los vamos a usar
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = false;
+//   }
+//   getTitulo() {
+//     return string(this.titulo).toLowerCase();
+//   }
+
+//   getAutor() {
+//     return string(this.autor).toLowerCase();
+//   }
+
+//   isDisponible() {
+//     if (true == this.disponible) {
+//       return `${this.titulo} está disponible`;
+//     } else {
+//       return `Lo sentimos, ${this.titulo} no está disponible actualmente`;
+//     }
+//   }
+
+//   prestar() {
+//     if (!this.disponible) {
+//       this.disponible = false;
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+
+//   devolver() {
+//     if (this.disponible) {
+//       this.disponible = true;
+//       return this.disponible;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+
+// const libro1 = new Libro(1, "El señor de los anillos", "J.R.R. Tolkien");
+
+// libro1.isDisponible();
+
+// ---
+
+// class Libro {
+//   constructor(id, titulo, autor, disponible) {
+//     this.id = id;
+//     this.titulo = titulo;
+//     this.autor = autor;
+//     this.disponible = disponible;
+//   }
+
+//   getTitulo() {
+//     return this.titulo;
+//   }
+//   getAutor() {
+//     return this.autor;
+//   }
+//   isDisponible(id) {
+//     if (id == 1) {
+//       return (this.disponible = true);
+//     } else {
+//       return (this.disponible = false);
+//     }
+//   }
+// }
+
+// const libro1 = new Libro(2, "Juventud", "anonimo", true);
+
+// const titulo = libro1.getTitulo();
+// const autor = libro1.getAutor();
+
+// console.log(titulo);
+// console.log(autor);
+// console.log(libro1.isDisponible(2));
+
+// ---
+
+class Libro {
+  constructor(id, titulo, autor) {
+    this.id = id;
+    this.titulo = titulo;
+    this.autor = autor;
+    this.disponible = true;
+  }
+  getTitulo() {
+    return this.titulo;
+  }
+  getAutor() {
+    return this.autor;
+  }
+  isDisponible() {
+    return this.disponible;
+  }
+  prestar() {
+    if (this.disponible) {
+      this.disponible = false;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  devolver() {
+    if (this.disponible) {
+      return false;
+    } else {
+      this.disponible = true;
+      return true;
+    }
+  }
+}
+
+const libro1 = new Libro(22, "Quijote", "Cervantes");
+
+// console.log(libro1.getTitulo());
+// console.log(libro1.getAutor());
+// console.log(libro1.isDisponible());
+
+// console.log("Prestar 1: " + libro1.prestar());
+// console.log(libro1.isDisponible());
+
+// console.log("Prestar 2: " + libro1.prestar());
+// console.log(libro1.isDisponible());
+
+// console.log("Devolver: " + libro1.devolver());
+// console.log(libro1.isDisponible());
+
+// ---
+
+// Crear clase `Biblioteca`:
+
+// - constructor(libros)
+// - getLibros()
+
+// ---
+
+class Biblioteca {
+  constructor(libros) {
+    this.libros = libros;
+  }
+}
+
+const libros = [
+  new Libro(1, "El Quijote", "Miguel de Cervantes"),
+  new Libro(2, "Cien años de soledad", "Gabriel García Márquez"),
+  new Libro(3, "La sombra del viento", "Carlos Ruiz Zafón"),
+  new Libro(4, "El amor en los tiempos del cólera", "Gabriel García Márquez"),
+  new Libro(5, "Don Juan Tenorio", "José"),
+];
+
+const bibliotecaPisoUno = new Biblioteca(libros);
+
+console.log(bibliotecaPisoUno);
